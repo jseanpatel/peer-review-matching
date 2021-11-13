dc4b592c9cc1be5a78776fd1d8178e74ed3f76aa
Jacob Patel <jseanpatel@gmail.com>
2021-11-13 13:10:21 -0800

    feat(SubmissionForm): added state for submission information

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -72,7 +72,7 @@ const Navbar = ({  }: Props) => {
                     Explore
                   </a>
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-600 transition duration-150 ease-in-out" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7" />
 </svg>
 
                   </div>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,48 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 
 import Navbar from "../components/Navbar";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [unitTopic, setUnitTopic] = useState("");
+  const [sessionLink, setSessionLink] = useState("");
+
+  useEffect(() => {
+    console.log(email);
+  }, [email]);
+
+  const submitForm = (event) => {
+    event.preventDefault();
+    console.log('email is ' + email)
+    console.log('unitTopic is ' + unitTopic)
+    console.log('sessionLink is ' + sessionLink)
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.currentTarget;
+    console.log("name is: " + value);
+
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "unitTopic") {
+      setUnitTopic(value);
+    } else if (name === "sessionLink") {
+      setSessionLink(value)
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center min-h-screen">
       <Head>
-        <title>C</title>
+        <title>Free Online Tutoring — Schoolhouse</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="preload"
+          href="/fonts/MuseoSansRounded"
+          as="font"
+          crossOrigin=""
+        />
       </Head>
       <Navbar> </Navbar>
       <main className="relative flex flex-col justify-center flex-1 w-full max-w-5xl px-20 pb-32 mx-auto text-left ">
@@ -16,73 +51,84 @@ export default function Home() {
         </h1>
 
         <h2 className="mt-3 text-xl">
-          In order to finalsize your submission for certification, please
+          In order to finalize your submission for certification, please
           complete the following form with your unit topic, email, and session
           link (either Loom or another service), and press submit.
         </h2>
 
-        <form class="w-full max-w-5xl mt-10 border rounded rounded-lg border-4 border-gray-300 p-10">
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+        <form
+          className="w-full max-w-5xl p-10 mt-10 border border-4 border-gray-300 rounded rounded-lg"
+          onSubmit={submitForm}
+        >
+          <div className="flex flex-wrap mb-6 -mx-3">
+            <div className="w-full px-3 mb-6 md:w-1/2 md:mb-0">
               <label
-                class="block tracking-wide text-gray-700 text-xl font-semibold mb-2"
+                className="block mb-2 text-xl font-semibold tracking-wide text-gray-700"
                 for="grid-unit-topic"
               >
                 Unit Topic:
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-red-500 rounded appearance-none focus:outline-none focus:bg-white"
                 id="grid-unit-topic"
+                name="unitTopic"
                 type="text"
+                onChange={(event) => onChangeHandler(event)}
                 placeholder="Algebra"
+                value={unitTopic}
               />
-              <p class="text-red-500 text-xs italic">
+              <p className="text-xs italic text-red-500">
                 Please fill out this field.
               </p>
             </div>
-            <div class="w-full md:w-1/2 px-3">
+            <div className="w-full px-3 md:w-1/2">
               <label
-                class="block tracking-wide text-gray-700 text-xl font-semibold mb-2"
+                className="block mb-2 text-xl font-semibold tracking-wide text-gray-700"
                 for="grid-email"
               >
                 Email:
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-email"
+                onChange={(event) => onChangeHandler(event)}
+                value={email}
+                name="email"
                 type="text"
                 placeholder="sal@schoolhouse.world"
               />
             </div>
           </div>
-          <div class="flex flex-wrap -mx-3 mb-6">
-            <div class="w-full px-3">
+          <div className="flex flex-wrap mb-6 -mx-3">
+            <div className="w-full px-3">
               <label
-                class="block tracking-wide text-gray-700 text-xl font-semibold mb-2"
+                className="block mb-2 text-xl font-semibold tracking-wide text-gray-700"
                 for="grid-session-link"
               >
                 Session Link:
               </label>
               <input
-                class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                className="block w-full px-4 py-3 mb-3 leading-tight text-gray-700 bg-gray-200 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-session-link"
                 type="url"
+                value={sessionLink}
+                onChange={(event) => onChangeHandler(event)}
+                name="sessionLink"
                 placeholder="https://loom.com/a"
               />
-              <p class="text-gray-600 text-xs italic">
-                Make it as long and as crazy as you'd like
-              </p>
+            
             </div>
           </div>
-          <div class="flex justify-end w-full">
-            <button class="bg-white justify-end hover:opacity-75 text-gray-400 text-lg border border-2 border-gray-400 font-bold py-0 px-8 rounded">
+          <div className="flex justify-end w-full">
+            <button className="justify-end px-8 py-0 text-lg font-bold text-gray-400 bg-white border border-2 border-gray-400 rounded hover:opacity-50">
               Clear
             </button>
-            <button class="bg-base justify-end ml-6 hover:bg-blue-700 text-white text-lg font-semibold py-0 px-6 rounded">
+            <button className="justify-end py-0 ml-6 text-lg font-semibold text-white rounded bg-base hover:bg-blue-700 px-7">
               Submit
             </button>
           </div>
         </form>
+        <div className="w-full h-[0.15rem] my-16 bg-gray-200"></div>
       </main>
     </div>
   );
