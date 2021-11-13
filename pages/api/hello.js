1c619fecb3a2b2fb0a8881e5b0c66e4e3f68af99
Jacob Patel <jseanpatel@gmail.com>
2021-11-13 13:32:52 -0800

    fix: submit image display

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default function testing(req, res) {
+  const {email, unitTopic, sessionLink } = req.body
+  res.status(200).json({ name: 'You have reached /testing' })
 }
+
+
--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Head from "next/head";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
+  const [submitted, setSubmitted] = useState();
   const [email, setEmail] = useState("");
   const [unitTopic, setUnitTopic] = useState("");
   const [sessionLink, setSessionLink] = useState("");
@@ -11,13 +12,6 @@ export default function Home() {
   useEffect(() => {
     console.log(email);
   }, [email]);
-
-  const submitForm = (event) => {
-    event.preventDefault();
-    console.log('email is ' + email)
-    console.log('unitTopic is ' + unitTopic)
-    console.log('sessionLink is ' + sessionLink)
-  };
 
   const onChangeHandler = (event) => {
     const { name, value } = event.currentTarget;
@@ -28,8 +22,31 @@ export default function Home() {
     } else if (name === "unitTopic") {
       setUnitTopic(value);
     } else if (name === "sessionLink") {
-      setSessionLink(value)
+      setSessionLink(value);
     }
+  };
+
+  const submitForm = (event) => {
+    event.preventDefault();
+
+    console.log("email is " + email);
+    console.log("unitTopic is " + unitTopic);
+    console.log("sessionLink is " + sessionLink);
+
+    fetch("/api/hello", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: email,
+        unitTopic: unitTopic,
+        sessionLink: sessionLink,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setSubmitted(true)
+      });
   };
 
   return (
@@ -46,15 +63,36 @@ export default function Home() {
       </Head>
       <Navbar> </Navbar>
       <main className="relative flex flex-col justify-center flex-1 w-full max-w-5xl px-20 pb-32 mx-auto text-left ">
-        <h1 className="pt-24 text-3xl font-normal text-start">
-          Thank you for completing your exam
-        </h1>
+        {!submitted ? (
+          <>
+            <h1 className="pt-24 text-3xl font-normal text-start">
+              Thank you for completing your exam
+            </h1>
 
-        <h2 className="mt-3 text-xl">
-          In order to finalize your submission for certification, please
-          complete the following form with your unit topic, email, and session
-          link (either Loom or another service), and press submit.
-        </h2>
+            <h2 className="mt-3 text-xl">
+              In order to finalize your submission for certification, please
+              complete the following form with your unit topic, email, and
+              session link (either Loom or another service), and press submit.
+            </h2>
+          </>
+        ) : (
+          <>
+           <img
+                  src={"success.svg"}
+                  alt="Submission was successful"
+                  className="w-16 h-16 mx-auto mt-8"
+                />
+            <h1 className="mt-3 text-3xl font-normal text-start">
+              Your submission was recieved!
+            </h1>
+
+            <h2 className="mt-3 text-xl">
+              As the final step in the certification process, we’d like to ask
+              you to review the submission of one other candidate. Please see
+              your candidate assignment below.
+            </h2>
+          </>
+        )}
 
         <form
           className="w-full max-w-5xl p-10 mt-10 border border-4 border-gray-300 rounded rounded-lg"
@@ -116,7 +154,6 @@ export default function Home() {
                 name="sessionLink"
                 placeholder="https://loom.com/a"
               />
-            
             </div>
           </div>
           <div className="flex justify-end w-full">
