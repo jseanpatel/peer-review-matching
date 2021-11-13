6b6ff51101cdc7e0600046c8021ecbf826a3087d
Jacob Patel <jseanpatel@gmail.com>
2021-11-13 02:50:47 -0800

    chore(Navbar): adjusted responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,15 +15,15 @@ const Navbar = ({  }: Props) => {
     <>
     <nav className="sticky top-0 z-50 bg-white">
       <div className="px-4 sm:px-6 md:px-6 lg:px-6">
-        <div className="relative flex items-center justify-between h-16">
-          <div className="absolute inset-y-0 right-0 flex items-center md:hidden">
+        <div className="relative flex items-center justify-center h-16">
+          <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
             <button
               onClick={toggle}
               aria-label="menu"
-              className="inline-flex items-center justify-center p-2 text-base transition duration-150 ease-in-out rounded-md hover:text-white hover:bg-orange-800 focus:outline-none focus:bg-orange-800 focus:text-white"
+              className="inline-flex items-center justify-center p-2 text-gray-600 transition duration-150 ease-in-out rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 focus:text-gray-600"
             >
               <svg
-                className="w-6 h-6"
+                className="w-5 h-5"
                 stroke="currentColor"
                 fill="none"
                 viewBox="0 0 24 24"
@@ -45,30 +45,30 @@ const Navbar = ({  }: Props) => {
               </svg>
             </button>
           </div>
-          <div className="absolute inset-y-0 left-0 flex items-center pr-2 md:static md:inset-auto md:pr-0">
+          <div className="absolute flex items-center pr-2 lg:static lg:inset-x-auto lg:pr-0">
             <Link href="/">
               <a>
                 {" "}
                 <img
                   src={"logo.svg"}
                   alt="Schoolhouse.world logo"
-                  className="hidden ml-2 w-44 lg:ml-0 sm:block"
+                  className="hidden ml-2 w-44 lg:ml-0 lg:block"
                 />
                  <img
                   src={"logo.svg"}
                   alt="Schoolhouse.world logo"
-                  className="block ml-4 w-44 sm:hidden"
+                  className="block ml-4 w-44 lg:hidden"
                 />
               </a>
             </Link>
           </div>
           <div className="flex items-center mr-auto">
-            <div className="hidden md:block md:ml-6">
+            <div className="hidden lg:block lg:ml-6">
               <div className="flex">
-                <Link href="/about">
+                <Link href="#explore">
                   <div className="flex flex-row items-center hover:text-blue-500 ">
 
-                  <a className="py-2 pl-4 pr-1 font-semibold leading-5 text-gray-600 transition duration-150 ease-in-out rounded-full text-md hover:text-base focus:outline-none focus:bg-orange-800">
+                  <a className="py-2 pl-4 pr-1 font-semibold leading-5 text-gray-600 transition duration-150 ease-in-out rounded-full text-md hover:text-base focus:outline-none ">
                     Explore
                   </a>
                   <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 text-gray-600 transition duration-150 ease-in-out" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -80,21 +80,21 @@ const Navbar = ({  }: Props) => {
                 </Link>
                 
 
-                <Link href="/our-work">
+                <Link href="#live-help">
                   <a className="px-4 py-2 ml-4 font-semibold text-gray-600 transition duration-150 ease-in-out rounded-full text-md focus:outline-none hover:text-base focus:bg-opacity-75">
                     {" "}
                     Live Help
                   </a>
                 </Link>
 
-                <Link href="/#contact-us">
+                <Link href="#about">
                   <a className="px-4 py-2 ml-4 font-semibold text-gray-600 transition duration-150 ease-in-out rounded-full text-md focus:outline-none hover:text-base focus:bg-opacity-75">
                     {" "}
                     About
                   </a>
                 </Link>
 
-                <Link href="/#contact-us">
+                <Link href="#faq">
                   <a className="px-4 py-2 ml-4 font-semibold text-gray-600 transition duration-150 ease-in-out rounded-full text-md focus:outline-none hover:text-base focus:bg-opacity-75">
                     {" "}
                     FAQ
@@ -105,15 +105,19 @@ const Navbar = ({  }: Props) => {
               </div>
             </div> 
           </div>
-          <div className="hidden md:block">
-
-          <Link href="/#contact-us">
+          <div className="block">
+          <Link href="#sign-in">
                   <a className="px-4 py-2 ml-auto text-base font-semibold transition duration-150 ease-in-out bg-white rounded-full text-md focus:outline-none hover:text-blue-600 focus:bg-opacity-75">
                     {" "}
                     Sign In
                   </a>
           </Link>
-          <Link href="/#contact-us">
+        </div>
+          
+          <div className="hidden sm:block">
+
+          
+          <Link href="#sign-up">
                   <a className="py-3 ml-auto font-semibold text-white transition duration-150 ease-in-out rounded rounded-lg px-7 text-md bg-base focus:outline-none hover:bg-blue-600 focus:bg-blue-600">
                     {" "}
                     Sign Up
@@ -126,28 +130,28 @@ const Navbar = ({  }: Props) => {
   
       <div className={isOpen ? "block" : "hidden"}>
         <div className="px-2 pt-2 pb-3 sm:ml-2">
-          <Link href="/about">
-            <a className="block px-4 py-2 mt-1 text-base font-semibold text-orange-800 transition duration-150 ease-in-out rounded-lg hover:text-white hover:bg-orange-800 focus:outline-none focus:text-white focus:bg-orange-800">
+          <Link href="#explore">
+            <a className="block px-4 py-2 mt-1 text-base font-semibold transition duration-150 ease-in-out focus:outline-none focus:text-white">
               Explore
             </a>
           </Link>
 
-          <Link href="/our-work">
-            <a className="block px-4 py-2 mt-1 text-base font-semibold text-orange-800 transition duration-150 ease-in-out rounded-lg hover:text-white hover:bg-orange-800 focus:outline-none focus:text-white focus:bg-orange-800">
+          <Link href="#live-help">
+            <a className="block px-4 py-2 mt-1 text-base font-semibold transition duration-150 ease-in-out rounded-lg hover:text-white focus:outline-none focus:text-white">
               {" "}
               Live help{" "}
             </a>
           </Link>
 
-          <Link href="/#contact-us">
-            <a className="block px-4 py-2 mt-1 text-base font-semibold text-orange-800 transition duration-150 ease-in-out bg-white rounded-lg hover:opacity-75 focus:outline-none focus:opacity-75">
+          <Link href="#about">
+            <a className="block px-4 py-2 mt-1 text-base font-semibold transition duration-150 ease-in-out bg-white rounded-lg hover:opacity-75 focus:outline-none focus:opacity-75">
               {" "}
               About
             </a>
           </Link>
 
-          <Link href="/#contact-us">
-            <a className="block px-4 py-2 mt-1 text-base font-bold text-orange-800 transition duration-150 ease-in-out bg-white rounded-lg hover:opacity-75 focus:outline-none focus:opacity-75">
+          <Link href="#faq">
+            <a className="block px-4 py-2 mt-1 text-base font-bold transition duration-150 ease-in-out bg-white rounded-lg hover:opacity-75 focus:outline-none focus:opacity-75">
               {" "}
               FAQ
             </a>
